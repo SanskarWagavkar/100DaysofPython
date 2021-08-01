21ad8275a668484397ea6b0f484ae1836c8aa8ee
SanskarWagavkar <80676415+SanskarWagavkar@users.noreply.github.com>
2021-08-02 01:38:51 +0530

    Add files via upload
    Aim :-  In this Program we have to take two input from the user and perform addition on it 
    level :-  Basic
    Learning Source :-  https://www.geeksforgeeks.org/python-programming-examples/

---
+++ Python program to add two numbers.py
@@ -0,0 +1,6 @@
+def add(a,b):
+    return print("Addition of %d + %d is %d"%(a,b,a+b))
+
+user_input_1 = int(input("Enter First Number :- "))
+user_input_2 = int(input("Enter Second Number :- "))
+add(user_input_1,user_input_2)
